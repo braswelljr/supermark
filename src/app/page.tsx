886eae7308b8dae87bac57a504cedc2c76a5aa2b
braswelljr <braswellkenneth7@gmail.com>
2023-09-30 12:25:53 +0000

    feat: add authentication function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { siteConfig } from '~/config/site'
+import { ThemeSwitch } from '~/components/ui/theme-switch'
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col justify-between">
@@ -6,6 +9,14 @@ export default function Home() {
           <section className="mx-auto max-w-7xl">hey</section>
         </main>
       </div>
+      <footer className="px-4 py-2 sm:px-8 sm:py-4">
+        <div className="mx-auto flex max-w-7xl items-center justify-between">
+          <h1 className="">
+            @{new Date().getFullYear()} {siteConfig.name}
+          </h1>
+          <ThemeSwitch className="" />
+        </div>
+      </footer>
     </div>
   )
 }
